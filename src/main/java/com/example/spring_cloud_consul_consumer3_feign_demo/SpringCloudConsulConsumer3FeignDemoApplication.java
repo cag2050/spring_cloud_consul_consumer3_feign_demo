e6edd1764a93be60666ce2acba53947cb0c018e4
chen_anguo <cag2050@163.com>
2019-09-16 19:56:30 +0800

    project init

---
+++ src/main/java/com/example/spring_cloud_consul_consumer3_feign_demo/SpringCloudConsulConsumer3FeignDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_cloud_consul_consumer3_feign_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudConsulConsumer3FeignDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudConsulConsumer3FeignDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/spring_cloud_consul_consumer3_feign_demo/SpringCloudConsulConsumer3FeignDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.spring_cloud_consul_consumer3_feign_demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringCloudConsulConsumer3FeignDemoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
