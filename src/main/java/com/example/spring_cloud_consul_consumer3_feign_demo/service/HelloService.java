95d33b4a6ce278fdef01cb52a5662e15f7cc89eb
chen_anguo <cag2050@163.com>
2019-09-17 10:25:33 +0800

    add: @RestController

--- src/main/java/com/example/spring_cloud_consul_consumer3_feign_demo/service/HelloService.java
+++ src/main/java/com/example/spring_cloud_consul_consumer3_feign_demo/service/HelloService.java
@@ -3,7 +3,7 @@ package com.example.spring_cloud_consul_consumer3_feign_demo.service;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@FeignClient(value = "service-producer")
+@FeignClient("service-producer")
 public interface HelloService {
     @RequestMapping("/hello")
     String hello();
